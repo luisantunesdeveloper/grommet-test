9f94c54b04ae6835f57be528ced5159ad25b0599
Luis Antunes <luisantunes.developer@gmail.com>
2017-01-22 08:43:26 +0000

    Add some page content

--- src/app.js
+++ src/app.js
@@ -11,20 +11,66 @@ import Anchor from 'grommet/components/Anchor';
 import Header from 'grommet/components/Header';
 import Footer from 'grommet/components/Footer';
 import Title from 'grommet/components/Title';
+import Article from 'grommet/components/Article';
+import Section from 'grommet/components/Section';
+import Search from 'grommet/components/Search';
+import Menu from 'grommet/components/Menu';
+import Heading from 'grommet/components/Heading';
+import Accordion from 'grommet/components/Accordion';
+import AccordionPanel from 'grommet/components/AccordionPanel';
+import Paragraph from 'grommet/components/Paragraph';
+
 
 render(
   <App centered={false}>
     <Box full={true}>
-      <Header direction='row' justify='between'
-        pad={{ horizontal: 'medium' }}>
-        <Title>Webpack 2 starter</Title>
-      </Header>
-      <Footer primary={true} appCentered={true} direction='column'
-        align='center' pad='small' colorIndex='grey-1'>
-        <p>
-          Build your ideas with <Anchor href='http://webpack2starter.io' target='_blank'>Webpack 2 starter</Anchor>!
-        </p>
-      </Footer>
+      <Article>
+        <Heading>
+          Sample Heading
+        </Heading>
+        <Header>
+          <Search inline={true} fill={true} size='medium' placeHolder='Search' dropAlign={{"right": "right"}} />
+          <Menu dropAlign={{"right": "right"}}>
+            <Anchor href='#'
+              className='active'>
+              First
+            </Anchor>
+            <Anchor href='#'>
+              Second
+            </Anchor>
+            <Anchor href='#'>
+              Third
+            </Anchor>
+          </Menu>
+        </Header>
+        <Section>
+          <Accordion>
+            <AccordionPanel heading='First Title'>
+              <Paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </Paragraph>
+            </AccordionPanel>
+            <AccordionPanel heading='Second Title'>
+              <Paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </Paragraph>
+            </AccordionPanel>
+            <AccordionPanel heading='Third Title'>
+              <Paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </Paragraph>
+            </AccordionPanel>
+          </Accordion>
+        </Section>
+        <Footer primary={true} appCentered={true} direction='column'
+          align='center' pad='small' colorIndex='grey-1'>
+          <p>
+            Build your ideas with <Anchor href='http://webpack2starter.io' target='_blank'>Webpack 2 starter</Anchor>!
+          </p>
+        </Footer>
+
+      </Article>
+
     </Box>
   </App>,
   document.getElementById('App')
